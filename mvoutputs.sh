90655c30d85fed68d5d1322d529168dd2e1a2c5e
bioatmosphere <wbwenwu@gmail.com>
2019-11-08 13:20:56 -0800

    added bash script mving files over to a folder

---
+++ mvoutputs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd /dDEMENTpy/output
+mv 2019110600.pickle ../../test_5
+cd ../..
+
+cd ../dDEMENTpy1/output
+mv 2019110601.pickle ../../test_5
+cd ../..
+
+cd ../dDEMENTpy2/output
+mv 2019110602.pickle ../../test_5
+cd ../..
+
+cd ../dDEMENTpy3/output
+mv 2019110603.pickle ../../test_5
+cd ../..
