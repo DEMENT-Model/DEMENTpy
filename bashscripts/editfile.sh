0a2d1350ed30446381feb0658e6558000e179ec6
bioatmosphere <wbwenwu@gmail.com>
2019-11-17 20:05:21 -0800

    updated

---
+++ bashscripts/editfile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+current="20191117"
+substitute="20191118"
+sed -i -e "s/$current/$substitute/" mvoutputs.sh
--- bashscripts/jupyternb.sh
+++ bashscripts/jupyternb.sh
@@ -9,4 +9,5 @@
 
 module load anaconda/3.7-5.3.0
 
+# --ExecutePreprocessor.timeout=180 important to the running of notebooks.
 jupyter nbconvert --ExecutePreprocessor.timeout=180 --to notebook --execute test_5.ipynb --output test_5.ipynb
