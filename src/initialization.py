2f35fc74adc6247fa0aa105515d0aa96adc1ac1c
bioatmosphere <wbwenwu@gmail.com>
2020-01-28 11:04:14 -0800

    trivial honing

--- src/initialization.py
+++ src/initialization.py
@@ -13,7 +13,6 @@ from microbe   import Microbe
 from utility   import expand
 
 
-
 def initialize_data(runtime_parameters):
     """
     Initialize all data required.
@@ -82,8 +81,10 @@ def initialize_data(runtime_parameters):
      
     #...an instance of Microbe class
     Microbes = Microbe(runtime_parameters,parameters)
-    #...Microbial community initialization#...note microbial_community is a tuple
-    microbial_community = Microbes.microbial_community_initialization()
+    #...Microbial pool initialization
+    microbial_pool = Microbes.microbial_pool_initialization()
+    #...Microbial community initialization
+    microbial_community = Microbes.microbial_community_initialization(microbial_pool[0],microbial_pool[1])
     #...Microbial minimum ratios
     microbial_min_ratios = Microbes.minimum_cell_quota()
     #...Microbial enzyme genes
@@ -101,62 +102,63 @@ def initialize_data(runtime_parameters):
     #...Microbial drought tolerance
     microbial_drought_tol = Microbes.microbe_drought_tol(microbial_osmolyte_prod_rate[2],microbial_osmolyte_prod_rate[3])
     #...Microbial mortality
-    microbial_mortality = Microbes.microbe_mortality(microbial_community[2])
+    microbial_mortality = Microbes.microbe_mortality(microbial_pool[1])
     
     #...Dump all initialized data into a dictionary; NOTE: variables with expand() put on the spatial grid
     gridsize = int(runtime_parameters.loc['gridsize',1])
     
-    Data_Dictionary = {"Substrates": expand(substrates_initial_pool,gridsize),
-                       "SubInput":   expand(substrates_input_rate,gridsize),
-                       "ReqEnz":           substrates_req_enzyme,
-                       "MonomersProduced": substrates_produced_monomers,
-                       "Monomers":     expand(monomers_initial_pool,gridsize),
-                       "Monomer_ratio":expand(monomer_ratio_inital,gridsize),
-                       "MonInput":     expand(monomers_input_rate,gridsize),
-                       "Uptake_ReqEnz":expand(monomers_uptake_reqenzyme,gridsize),
-                       "Enzymes":      expand(enzymes_initial_pool,gridsize),
-                       "Km0":          expand(enzymes_Km,gridsize),            # enzyme half-saturation constant
-                       "Uptake_Km0":   expand(enzymes_uptake_Km,gridsize),     # transporter half-saturation constant
-                       "Uptake_Ea":    expand(enzymes_uptake_Ea,gridsize),     # transporter acitivation energy
-                       "Uptake_Vmax0": expand(enzymes_uptake_Vmax,gridsize),   # transporter reaction rate
-                       "Ea":           expand(enzymes_Ea,gridsize),            # enzyme activation energy
-                       "Vmax0":        expand(enzymes_Vmax_T,gridsize),        # enzyme reaction rate
-                       "EnzAttrib":    enzymes_attributes,                     # enzyme stoichiometry and energy cost
-                       "Microbes_pp": microbial_community[0],                  # tuple[0]: microbes preceding placement
-                       "Microbes":    microbial_community[1],                  # tuple[1]: initialized spatial microbes
-                       "fb":          microbial_community[2],                  # tuple[2]: fungi index
-                       "Bac_density": microbial_community[3],                  # tuple[3]: bacterial density
-                       "Fun_density": microbial_community[4],                  # tuple[4]: fungi density
-                       "MinRatios":   expand(microbial_min_ratios,gridsize),   # microbial cell min. ratios
-                       "UptakeGenes": expand(microbial_uptake_gene,gridsize),  # transporter gene distribution across taxa
-                       "OsmoGenes":   expand(microbial_osmolyte_gene,gridsize),# osmolyte gene distribution across taxa
-                       "EnzGenes":    expand(microbial_enzyme_gene,gridsize),  # enzyme gene distribution across taxa
-                       "UptakeGenes_trait":   expand(microbial_uptake_cost[0],gridsize),        # single gene cost of transporter
-                       "OsmoProdConsti_trait":expand(microbial_osmolyte_prod_rate[0],gridsize), # single gene cost of constitutive osmolyte
-                       "OsmoProdInduci_trait":expand(microbial_osmolyte_prod_rate[1],gridsize), # single gene cost of inducible osmolyte
-                       "EnzProdConsti_trait": expand(microbial_enzyme_prod_rate[0],gridsize),   # single gene cost of constitutive enzyme
-                       "EnzProdInduci_trait": expand(microbial_enzyme_prod_rate[1],gridsize),   # single gene cost of inducible enzyme
-                       "UptakeGenesCost":     expand(microbial_uptake_cost[1],gridsize),        # distribution of transporter gene cost across taxa
-                       "OsmoProdConsti":      expand(microbial_osmolyte_prod_rate[2],gridsize), # distribution of consti. osmolyte gene cost across taxa
-                       "OsmoProdInduci":      expand(microbial_osmolyte_prod_rate[3],gridsize), # distribution of induci. osmolyte gene cost across taxa
-                       "EnzProdConstit":      expand(microbial_enzyme_prod_rate[2],gridsize),   # distribution of consti. enzyme gene cost across taxa
-                       "EnzProdInduce":       expand(microbial_enzyme_prod_rate[3],gridsize),   # distribution of induci. enzyme gene cost across taxa
-                       "TaxDroughtTol":       expand(microbial_drought_tol,gridsize),           # distribution of taxon-specific drought tol.
-                       'basal_death_prob':  microbial_mortality[0],                # basal death probability
-                       'death_rate':        microbial_mortality[1],                # change rate of death prob. agaist mositure
-                       "AE_ref":            parameters.loc["CUE_ref",1],           # Reference assimilation efficiency: 0.5
-                       "AE_temp":           parameters.loc["CUE_temp",1],          # AE temperature sensitivity; default: -0.016
-                       'Uptake_Maint_cost': parameters.loc['Uptake_Maint_cost',1], # constant of transporter maintenence cost
-                       'C_min':             parameters.loc['C_min',1],             # C threshold of cell lysis
-                       'N_min':             parameters.loc['N_min',1],             # N threshold of cell lysis
-                       'P_min':             parameters.loc['P_min',1],             # P threshold of cell lysis
-                       'max_size_b':        parameters.loc['max_size_b',1],        # C quota threshold for bacterial cell division
-                       'max_size_f':        parameters.loc['max_size_f',1],        # C quota threshold for fungal cell division
-                       'wp_fc':             parameters.loc['wp_fc',1],             # threshold below which microbes start to respond to drought
-                       'wp_th':             parameters.loc['wp_th',1],             # threshold below which microbes in full swing to respond to drought
-                       'alpha':             parameters.loc['alpha',1],             # factor delineating curve concavity of microbial response to drought
-                       'Temp': daily_temp,                                         # temperature
-                       'Psi':  daily_psi                                           # water potential
-                      }
+    Data_Dictionary = {
+        "Substrates": expand(substrates_initial_pool,gridsize),
+        "SubInput":   expand(substrates_input_rate,gridsize),
+        "ReqEnz":           substrates_req_enzyme,
+        "MonomersProduced": substrates_produced_monomers,
+        "Monomers":     expand(monomers_initial_pool,gridsize),
+        "Monomer_ratio":expand(monomer_ratio_inital,gridsize),
+        "MonInput":     expand(monomers_input_rate,gridsize),
+        "Uptake_ReqEnz":expand(monomers_uptake_reqenzyme,gridsize),
+        "Enzymes":      expand(enzymes_initial_pool,gridsize),
+        "Km0":          expand(enzymes_Km,gridsize),            # enzyme half-saturation constant
+        "Uptake_Km0":   expand(enzymes_uptake_Km,gridsize),     # transporter half-saturation constant
+        "Uptake_Ea":    expand(enzymes_uptake_Ea,gridsize),     # transporter acitivation energy
+        "Uptake_Vmax0": expand(enzymes_uptake_Vmax,gridsize),   # transporter reaction rate
+        "Ea":           expand(enzymes_Ea,gridsize),            # enzyme activation energy
+        "Vmax0":        expand(enzymes_Vmax_T,gridsize),        # enzyme reaction rate
+        "EnzAttrib":    enzymes_attributes,                     # enzyme stoichiometry and energy cost
+        "Microbes_pp": microbial_pool[0],                       # tuple[0]: microbes preceding placement
+        "fb":          microbial_pool[1],                       # tuple[1]: fungi index
+        "Microbes":    microbial_community[0],                  # tuple[0]: initialized spatial microbes
+        "Bac_density": microbial_community[1],                  # tuple[1]: initialized bacterial density
+        "Fun_density": microbial_community[2],                  # tuple[2]: initialized fungal density
+        "MinRatios":   expand(microbial_min_ratios,gridsize),   # microbial cell min. ratios
+        "UptakeGenes": expand(microbial_uptake_gene,gridsize),  # transporter gene distribution across taxa
+        "OsmoGenes":   expand(microbial_osmolyte_gene,gridsize),# osmolyte gene distribution across taxa
+        "EnzGenes":    expand(microbial_enzyme_gene,gridsize),  # enzyme gene distribution across taxa
+        "UptakeGenes_trait":   expand(microbial_uptake_cost[0],gridsize),        # single gene cost of transporter
+        "OsmoProdConsti_trait":expand(microbial_osmolyte_prod_rate[0],gridsize), # single gene cost of constitutive osmolyte
+        "OsmoProdInduci_trait":expand(microbial_osmolyte_prod_rate[1],gridsize), # single gene cost of inducible osmolyte
+        "EnzProdConsti_trait": expand(microbial_enzyme_prod_rate[0],gridsize),   # single gene cost of constitutive enzyme
+        "EnzProdInduci_trait": expand(microbial_enzyme_prod_rate[1],gridsize),   # single gene cost of inducible enzyme
+        "UptakeGenesCost":     expand(microbial_uptake_cost[1],gridsize),        # distribution of transporter gene cost across taxa
+        "OsmoProdConsti":      expand(microbial_osmolyte_prod_rate[2],gridsize), # distribution of consti. osmolyte gene cost across taxa
+        "OsmoProdInduci":      expand(microbial_osmolyte_prod_rate[3],gridsize), # distribution of induci. osmolyte gene cost across taxa
+        "EnzProdConstit":      expand(microbial_enzyme_prod_rate[2],gridsize),   # distribution of consti. enzyme gene cost across taxa
+        "EnzProdInduce":       expand(microbial_enzyme_prod_rate[3],gridsize),   # distribution of induci. enzyme gene cost across taxa
+        "TaxDroughtTol":       expand(microbial_drought_tol,gridsize),           # distribution of taxon-specific drought tol.
+        'basal_death_prob':  microbial_mortality[0],                # basal death probability
+        'death_rate':        microbial_mortality[1],                # change rate of death prob. agaist mositure
+        "AE_ref":            parameters.loc["CUE_ref",1],           # Reference assimilation efficiency: 0.5
+        "AE_temp":           parameters.loc["CUE_temp",1],          # AE temperature sensitivity; default: -0.016
+        'Uptake_Maint_cost': parameters.loc['Uptake_Maint_cost',1], # constant of transporter maintenence cost
+        'C_min':             parameters.loc['C_min',1],             # C threshold of cell lysis
+        'N_min':             parameters.loc['N_min',1],             # N threshold of cell lysis
+        'P_min':             parameters.loc['P_min',1],             # P threshold of cell lysis
+        'max_size_b':        parameters.loc['max_size_b',1],        # C quota threshold for bacterial cell division
+        'max_size_f':        parameters.loc['max_size_f',1],        # C quota threshold for fungal cell division
+        'wp_fc':             parameters.loc['wp_fc',1],             # threshold below which microbes start to respond to drought
+        'wp_th':             parameters.loc['wp_th',1],             # threshold below which microbes in full swing to respond to drought
+        'alpha':             parameters.loc['alpha',1],             # factor delineating curve concavity of microbial response to drought
+        'Temp': daily_temp,                                         # temperature
+        'Psi':  daily_psi                                           # water potential
+    }
 
     return Data_Dictionary
